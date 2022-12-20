88598003c811ba34e6ab465b780dddc19ad1ebfa
MonikaS132 <monikasekar32@gmail.com>
2022-12-20 11:52:35 +0530

    C:Usersmonikeclipse-workspaceGitProgram

---
+++ src/org/test/Sample.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Sample {
+	
+	public void git() {
+		System.out.println("welcome to Git");
+	}
+	
+	public static void main(String[] args) {
+		Sample s=new Sample();
+		s.git();
+	}
+
+}
